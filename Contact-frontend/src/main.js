ec0585eb89202b3e6af78df8a34366b5cc6de3cb
Gael Lobato <gaelsanchez509@gmail.com>
2025-07-31 08:36:30 -0500

    Dist actulizado

--- Contact-frontend/src/main.js
+++ Contact-frontend/src/main.js
@@ -7,6 +7,6 @@ import { install } from 'vue3-recaptcha-v2';
 createApp(App)
   .use(router)
   .use(install, {
-    sitekey: '6LexOY8rAAAAAFIEFYwxj9vppIlRstvp6p6_Z8f4',
+    sitekey: '6LdgMpUrAAAAABD3LOFuG1x6BqrGgtNsb5gFGJft',
   })
   .mount('#app');
